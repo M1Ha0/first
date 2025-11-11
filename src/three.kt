68d179c86123704391c5ad19135c1d00390606b9
mikhailymikhail@gmail.com <mikhailymikhail@gmail.com>
2025-11-11 12:59:48 +0200

    three

--- src/three.kt
+++ src/three.kt
@@ -7,15 +7,26 @@ fun main() {
     val line = Line(left, right)
     line.squer()
     //11.2
+    print("Введите число для проверки: ")
+    val x = readLine()!!.toDouble()
+    val line1 =Chek(x,left,right)
+    line1.chek()
 }
 //11.1
-class Line(val left:Double,  val right:Double){
+open class Line(val left:Double,  val right:Double){
 
     val s = right - left
     fun squer()=println(s*s)
 }
 //11.2
-//class Chek(val x:Double) : Line(left,right){
-//
-//}
+class Chek(val x:Double, left:Double,  right:Double) : Line(left,right){
+    fun chek() {
+        if (x>left&&x<right){
+                println("Число $x входит в диапозон [$left;$right]")
+        } else{
+                println("Число $x не входит в диапозон [$left;$right]")
+        }
+    }
+}
+//11.3
 
